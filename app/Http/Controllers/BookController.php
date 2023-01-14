a1fdbf24ce6719e9f9425b35174af0ba1a316a81
Pedro Augusto <pedrocaaugusto@gmail.com>
2023-01-14 15:11:46 -0300

    feat(books): :sparkles: Create controller for books

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreBookRequest;
 use App\Http\Requests\UpdateBookRequest;
 use App\Models\Book;
+use Validator;
 
 class BookController extends Controller
 {
+
     /**
      * Display a listing of the resource.
      *
@@ -15,17 +17,8 @@ class BookController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        $books = Book::paginate(5);
+        return response()->json($books, 200);
     }
 
     /**
@@ -36,7 +29,18 @@ class BookController extends Controller
      */
     public function store(StoreBookRequest $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'isbn' => 'required|numeric|max:',
+            'value' => 'required|numeric',
+        ]);
+        
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $book = Book::create($validator->validated());
+        return response()->json($book, 201);
     }
 
     /**
@@ -47,18 +51,7 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Book  $book
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Book $book)
-    {
-        //
+        return response()->json($book, 200);
     }
 
     /**
@@ -70,7 +63,18 @@ class BookController extends Controller
      */
     public function update(UpdateBookRequest $request, Book $book)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'string|max:255',
+            'isbn' => 'numeric|max:',
+            'value' => 'numeric',
+        ]);
+        
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $book->update($validator->validated());
+        return response()->json($book, 200);
     }
 
     /**
@@ -81,6 +85,7 @@ class BookController extends Controller
      */
     public function destroy(Book $book)
     {
-        //
+        $book->delete();
+        return response()->json(null, 204);
     }
 }
